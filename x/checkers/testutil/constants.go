06c6d1c02d861e41498bfa17bf1c35fc0cf4f340
Wael Almattar <waelsy123@gmail.com>
2023-01-02 01:40:54 +0100

    another custom test

---
+++ x/checkers/testutil/constants.go
@@ -0,0 +1,7 @@
+package testutil
+
+const (
+	Alice = "cosmos1dl9ursqncfaskz3l5gnexacq0p9urxk6x3g5cl"
+	Bob   = "cosmos1nqppg4aylpsc0jl960d2gtq7smn7azmezelezk"
+	Wael  = "cosmos1jv6g2s7pvx2yjffmn9ltzmyfq9fjdx0yvgkx56"
+)

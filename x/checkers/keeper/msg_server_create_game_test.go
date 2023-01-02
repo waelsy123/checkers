85caab2735df68747bc257e2552e0657d579a4cc
Wael Almattar <waelsy123@gmail.com>
2023-01-02 20:11:48 +0100

    create game tests

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,15 +1,26 @@
 package keeper_test
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
+	keepertest "github.com/waelsy123/checkers/testutil/keeper"
+	"github.com/waelsy123/checkers/x/checkers"
+	"github.com/waelsy123/checkers/x/checkers/keeper"
 	"github.com/waelsy123/checkers/x/checkers/types"
 )
 
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
+
 func TestCreateGame(t *testing.T) {
-	msgServer, context := setupMsgServer(t)
+	msgServer, _, context := setupMsgServerCreateGame(t)
 	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
@@ -19,6 +30,61 @@ func TestCreateGame(t *testing.T) {
 
 	require.Nil(t, err)
 	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "", // TODO: update with a proper value when updated
+		GameIndex: "1", // TODO: update with a proper value when updated
 	}, *createResponse)
+}
+
+func TestCreate1GameHasSaved(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     wael,
+	})
+	systemInfo, found := keeper.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId: 2,
+	}, systemInfo)
+	game1, found1 := keeper.GetStoredGame(ctx, "1")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Index: "1",
+		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:  "b",
+		Black: bob,
+		Red:   wael,
+	}, game1)
+}
+
+func TestCreate1GameGetAll(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     wael,
+	})
+	games := keeper.GetAllStoredGame(sdk.UnwrapSDKContext(context))
+	require.Len(t, games, 1)
+	require.EqualValues(t, types.StoredGame{
+		Index: "1",
+		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:  "b",
+		Black: bob,
+		Red:   wael,
+	}, games[0])
+}
+
+func TestCreateGameRedAddressBad(t *testing.T) {
+	msgServer, _, context := setupMsgServerCreateGame(t)
+	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     "notanaddress",
+	})
+	require.Nil(t, createResponse)
+	require.Equal(t,
+		"red address is invalid: notanaddress: decoding bech32 failed: invalid separator index -1",
+		err.Error())
 }
